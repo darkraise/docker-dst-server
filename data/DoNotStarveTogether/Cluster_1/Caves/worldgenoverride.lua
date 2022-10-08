e901291d4c24562105ee73cdfd2f59e1757a499d
Darkraise <quangtc94@gmail.com>
2022-10-08 16:55:40 +0700

    Add Default data for Darkraise server

---
+++ data/DoNotStarveTogether/Cluster_1/Caves/worldgenoverride.lua
@@ -0,0 +1,8 @@
+return {
+	override_enabled = true,
+	worldgen_preset = "DST_CAVE_PLUS",
+	settings_preset = "DST_CAVE_PLUS",
+	overrides = {
+		berrybush = "often",
+	},
+}
---
+++ data/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
@@ -0,0 +1,10 @@
+return {
+	override_enabled = true,
+	worldgen_preset = "SURVIVAL_TOGETHER",
+	settings_preset = "SURVIVAL_TOGETHER",
+	overrides = {
+		branching = "never",
+		loop = "never",
+		start_location = "plus",
+	},
+}
